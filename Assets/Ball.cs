8c2196d92120e4a9d6d9909e4611a274a0fc9caa
AGhafori <aghafori@hotmail.com>
2017-10-13 14:21:07 -0500

    Assets

---
+++ Assets/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float moveSpeed = 25f;
+
+    private Rigidbody rbody;
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+
+        rbody.velocity = new Vector3(0f, 15f, 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.name == "wall1")
+        {
+            //print score for player 1
+        }
+        else if (col.collider.name == "wall2")
+        {
+            //print score for player 2
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float paddleSpeed = 10f;
+    
+    private Rigidbody pbody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        pbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        float moveX = inputX * paddleSpeed * Time.deltaTime;
+        float moveY = inputY * paddleSpeed * Time.deltaTime;
+
+        pbody.AddForce(moveX, moveY, 0f);
+
+    }
+}
